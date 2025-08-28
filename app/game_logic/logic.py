5afa8a979ca976a4bd8c24652296547eb14719de
Joseph-Hansel <abogejosephhansel@gmail.com>
2025-08-28 17:17:43 +0300

    post second merge edits (development)

---
+++ app/game_logic/logic.py
@@ -0,0 +1,15 @@
+class GameLogic:
+    def __init__(self, board):
+        self.board = board
+
+    def reveal(self, row, col):
+        if (row, col) in self.board.mine_positions:
+            return False  # lose
+        self.board.grid[row][col] = "⬜"
+        return True
+
+    def check_win(self):
+        return all(
+            (r, c) in self.board.mine_positions or self.board.grid[r][c] != "🟫"
+            for r in range(self.board.size) for c in range(self.board.size)
+        )
